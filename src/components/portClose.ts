291a944e9441376e18270a86b3d178189dee05c4
konjikun <shintaniallen@gmail.com>
2023-09-18 00:38:26 +0900

    edit servo for consecutive

--- src/components/portClose.ts
+++ src/components/portClose.ts
@@ -2,6 +2,7 @@ import { SerialPort } from "serialport";
 
 // This function closes the serial port. Console will be closed.
 export const portClose = (port: SerialPort): Promise<void> => {
+  console.log('Closing port');
   return new Promise((resolve, reject) => {
     port.close(err => {
       if (err) {
--- src/test/Servo.ts
+++ src/test/Servo.ts
@@ -1,3 +1,12 @@
 import { Servo } from '../Servo';
+import { findArduinoPath } from '../components/findArduinoPath';
+import { delay } from '../components/delay';
 
-Servo(8, 90);
+const main = async () => {
+    const path = await findArduinoPath();
+    await Servo(path, 8, 0);
+    await delay(100);
+    await Servo(path, 8, 90);
+}
+
+main();
