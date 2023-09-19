bad9ccfe60bafa602e868263646262e400c29bbc
konjikun <shintaniallen@gmail.com>
2023-09-19 21:29:46 +0900

    add buzzer

--- src/utils/setup.ts
+++ src/utils/setup.ts
@@ -1,9 +1,7 @@
 import { SerialPort } from "serialport";
-import { setPinToServo } from "../helper/Servo/setPinToServo";
 import { findArduinoPath } from "./findArduinoPath";
-import { setServoAngle } from "../helper/Servo/setServoAngle";
-import { delay } from "./delay";
-import { setLedState } from "../components/led/LED";
+import { outputPort } from "../factory/output/led";
+import { servoPort } from "../factory/servo/servo";
 
 const setup = async () => {
     //console.log(1)
@@ -23,37 +21,12 @@ const setup = async () => {
            onDataReady();
          }
         })
-
         await dataReady
-
-  const servo = (pin: number) => {
-    return {
-      rotate: async (angle: number) => {
-        //console.log(4)
-        await setPinToServo(pin, port);
-        await setServoAngle(pin, angle, port);
-
-        //console.log(`Rotating to ${angle} degrees`);
-        const DELAY_TIME = 290 +(Math.abs(angle - 90) * 2.5);
-        await delay(DELAY_TIME);
-      }
-    };
-  };
-  
-  const led = (pin: number) => {
-    return {
-      on: async () => {
-        await setLedState(pin, true, port);
-      },
-      off: async () => {
-        await setLedState(pin, false, port);
-      }
-    };
-  };
-  
+        
   return {
-    servo,
-    led
+    servo: servoPort(port),
+    led: outputPort(port),
+    buzzer: outputPort(port)
   };
 }
 
