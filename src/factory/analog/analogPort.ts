9ccb5caf6c8d4a5da5c6350d3336d0af6f8287b3
konjikun <shintaniallen@gmail.com>
2023-09-22 22:52:07 +0900

    add Observation of rxjs

--- src/factory/analog/analogPort.ts
+++ src/factory/analog/analogPort.ts
@@ -1,11 +1,31 @@
 import { SerialPort } from "serialport";
 import { setAnalogState } from "../../uniqueDevice/setAnalogState";
+import { AnalogPin, analogPinMapping } from "../../types/analog/analog";
+import { Subscription } from "rxjs";
 
 export const analogPort = (port: SerialPort) => {
-    return (pin: number) => {
+    return (analogPin: AnalogPin) => {
+        const pin = analogPinMapping[analogPin];
+        let subscription:Subscription;
+
         return {
-            read: async () => {
-                await setAnalogState(pin, port);
+            read: async (func:Promise<void> | void | (() => any)):Promise<void> => {
+                const observable = setAnalogState(pin, port);
+                subscription = observable.subscribe(value => {
+                    if (value) {
+                        // 圧力がかかった場合の処理をこちらに書きます
+                        console.log(value);
+                        func
+                    } else {
+                        // 圧力がかからなかった場合の処理をこちらに書きます
+                        console.log(value);
+                    }
+                });
+            },
+            stop: () => {
+                if (subscription) {
+                    subscription.unsubscribe();
+                }
             }
         }
     }
--- src/test/pressierSensor.ts
+++ src/test/pressierSensor.ts
@@ -2,7 +2,7 @@ import setup from "../utils/setup"
 const main = async () => {
     const port = await setup()
 
-    const sensor1 = port.pressureSensor(0)
+    const sensor1 = port.pressureSensor('A0')
     await sensor1.read()
 }
 
