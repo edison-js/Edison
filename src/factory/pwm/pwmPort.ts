5a482264995f2900e1a92141623fc21c923fcc0d
konjikun <shintaniallen@gmail.com>
2023-10-17 03:37:43 +0900

    add RGB-LED

---
+++ src/factory/pwm/pwmPort.ts
@@ -0,0 +1,15 @@
+import { SerialPort } from 'serialport'
+import { setPwmState } from '../../helper/PWM/setPwmState'
+
+export const pwmPort = (port: SerialPort) => {
+  return (pin: number) => {
+    return {
+      analogWrite: async (value: number) => {
+        await setPwmState(pin, value, port)
+      },
+      off: async () => {
+        await setPwmState(pin, 0, port) // PWM値を0に設定してLEDをオフにする
+      },
+    }
+  }
+}
