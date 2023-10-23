d55a88e650d5964ff5efe2f6e8caed467166de62
konjikun <shintaniallen@gmail.com>
2023-10-24 01:39:39 +0900

    add hall effect sensor

--- src/test/HallEffectSensor.ts
+++ src/test/HallEffectSensor.ts
@@ -1,11 +1,16 @@
-import { board } from '../utils/board'
 import { SerialPort } from 'serialport'
-// import { attachHallEffectSensor } from '../factory/input/uniqueDevice/hallEffectSensor'
+import { attachLed } from '../factory/output/uniqueDevice/led'
+import { board } from '../utils/board'
+import { attachHallEffectSensor } from '../factory/input/uniqueDevice/hallEffectSensor'
 
-// board.connectManual('/dev/ttyUSB0')
+board.connectManual('/dev/ttyUSB0')
 
-// board.on('ready', async (port: SerialPort) => {
-//   console.log('Board is ready!')
-//   const hallEffectSensor = attachHallEffectSensor(port, 12)
-//   await hallEffectSensor.on()
-// })
+board.on('ready', (port: SerialPort) => {
+  console.log('Board is ready!')
+  const led1 = attachLed(port, 13)
+
+  const hallEffectSensor = attachHallEffectSensor(port, 12)
+  hallEffectSensor.read('on', () => {
+    led1.on()
+  })
+})
