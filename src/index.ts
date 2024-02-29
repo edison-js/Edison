f3e2fcfe38d2bd62fa2777d372212b9a9bd8dd0b
1f10220123 <s1f102201234@iniad.org>
2024-02-29 16:58:32 +0900

    Add device in index.ts

--- src/index.ts
+++ src/index.ts
@@ -20,6 +20,9 @@ export { Buzzer } from './declarative/components/output/Buzzer'
 export { Button } from './declarative/components/input/Button'
 export { Collision } from './declarative/components/input/Collision'
 export { HallEffective } from './declarative/components/input/HallEffectSensor'
+export { IRReceiver } from './declarative/components/input/IRReceiver'
+export { PIRMotionSensor } from './declarative/components/input/PIRMotionSensor'
 export { Servo } from './declarative/components/servo/Servo'
 
 export type { SerialPort } from 'serialport'
+ 
