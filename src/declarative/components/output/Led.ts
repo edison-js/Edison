f9d6b8c3bb59595a46f70c5f1b05e6911ef18576
1f10220123 <s1f102201234@iniad.org>
2024-03-27 12:30:19 +0900

    fix LED files

--- src/declarative/components/output/Led.ts
+++ src/declarative/components/output/Led.ts
@@ -1,6 +1,6 @@
 import type React from 'react'
 import { board } from '../../../procedure/utils/board'
-import { attachOutput } from '../../../procedure/examples/output/uniqueDevice/output'
+import { attachLed } from '@/procedure/examples/output/uniqueDevice/led'
 
 type LEDProps = {
   pin: number
@@ -17,7 +17,7 @@ const setupLed = (props: LEDProps) => {
     return
   }
 
-  const led = attachOutput(port, pin)
+  const led = attachLed(port, pin)
 
   if (isOn === true) {
     led.on()
