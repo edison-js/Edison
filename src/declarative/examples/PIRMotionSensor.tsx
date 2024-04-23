1e78e581efcd623b039bfca78e76bb5e9e6aadbc
konjikun <shintaniallen@gmail.com>
2024-04-23 20:24:20 +0900

    adjust to document

--- src/declarative/examples/PIRMotionSensor.tsx
+++ src/declarative/examples/PIRMotionSensor.tsx
@@ -3,14 +3,14 @@ import { useState } from 'react'
 import { Board } from '../utils/Board'
 import { render } from '../rendere/render'
 import { Led } from '../components/output/Led'
-import { PIRMotionSensor } from '../components/input/PIRMotionSensor'
+import { PIRMotion } from '../components/input/PIRMotionSensor'
 
 const App: React.FC = () => {
   const [isOn, setIsOn] = useState(false)
 
   return (
     <Board port={'/dev/ttyUSB0'}>
-      <PIRMotionSensor
+      <PIRMotion
         pin={8}
         triggered={() => {
           setIsOn(true)
@@ -24,7 +24,7 @@ const App: React.FC = () => {
           pin={13}
           isOn={isOn}
         />
-      </PIRMotionSensor>
+      </PIRMotion>
     </Board>
   )
 }
